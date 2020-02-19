bac404b4da4872b7a8453dd496c049b0633e517a
Hailey Koo <n01382791@nj-00118-006.humber.org>
2020-02-18 19:03:13 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require ("express")
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');   
+
+const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/products', function (req, res) {
+    res.render('products');
+});
+
+app.get('/contact', function (req, res) {
+    res.render('contact');
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log (`web server is running`)
+}
+)
